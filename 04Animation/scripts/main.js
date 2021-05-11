1d9392e29ccacddf87bc6847d82c8f4bf499900c
Suzy90 <18030033@student.hhs.nl>
2021-05-11 17:17:47 +0200

    04_Animation

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,7 +5,7 @@ var rotationSpeed = 0.01;
 myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
- myOtherBox.object3D.rotation.x += rotationSpeed;
+ myOtherBox.object3D.rotation.x -= rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
 
 setInterval(spin, 16); //equivalent to 60 fps
