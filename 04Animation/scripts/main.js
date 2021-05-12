33ab18fac8354b35c33882b9fd6c8277d6d46990
Suzy90 <18030033@student.hhs.nl>
2021-05-12 15:03:56 +0200

    04_Animation

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -11,3 +11,14 @@ console.log(myOtherBox.object3D.rotation.x);
 
 setInterval(spin, 16); //equivalent to 60 fps
 
+function bigBox(x) {
+  x.style.height = "3";
+  x.style.width = "9";
+   x.style.depth = "3";
+}
+
+function normalBox(x) {
+  x.style.height = "1";
+  x.style.width = "3";
+  x.style.depth = "1";
+}
