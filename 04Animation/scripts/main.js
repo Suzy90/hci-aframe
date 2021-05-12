ce55200bcc51efa4d0e189a70369d2d942c73bff
Suzy90 <18030033@student.hhs.nl>
2021-05-12 15:42:41 +0200

    04_Animation

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -11,12 +11,3 @@ console.log(myOtherBox.object3D.rotation.x);
 
 setInterval(spin, 16); //equivalent to 60 fps
 
-document.getElementById("demo").addEventListener("mouseenter", mouseEnter);
-document.getElementById("demo").addEventListener("mouseleave", mouseLeave);
-
-function mouseEnter() {
-  document.getElementById("demo").style.color = "metal";
-}
-
-function mouseLeave() {
-  document.getElementById("demo").style.color = "wood";
